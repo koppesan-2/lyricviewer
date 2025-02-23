5af7d40046ede4105ea2f0e3643f11af65f6e241
koppesan-2 <koppesan@outlook.jp>
2025-02-24 01:07:00 +0900

    権限取得タイミングの変更

--- js/service-worker.js
+++ js/service-worker.js
@@ -2,11 +2,10 @@ self.addEventListener('fetch', function (e) {
     console.log('service worker fetch')
 })
 self.addEventListener('install', function (e) {
+})
+self.addEventListener('activate', function (e) {
     console.log('service worker install')
     Notification.requestPermission().then((result) => {
         console.log(result);
     });
-})
-self.addEventListener('activate', function (e) {
-    console.log('service worker activate')
 })
