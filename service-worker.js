6175c4b49cddbdfd1a1bee5fbafb77db79bfc7a4
koppesan-2 <koppesan@outlook.jp>
2025-02-24 12:18:23 +0900

    どこでも使えるURLに変更

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('message', event => {
     if (event.data !== null) {
       void self.registration.showNotification(event.data, {
         body: 'PWA Sample notification.',
-        icon: window.location.href.indexOf("https://koppesan-2github.io")==-1?"/img/icons/circle-play.svg":"/lyricviewer/img/icons/circle-play.svg"
+        icon: "https://koppesan-2.hithub.io/lyricviewer/img/icons/pen.svg"
       })
     }
   })
