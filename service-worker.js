96a380a834b16bfd248d7b6f6b377e2a22848da1
koppesan-2 <koppesan@outlook.jp>
2025-02-24 12:05:25 +0900

    条件分岐付きアイコン

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ self.addEventListener('message', event => {
     if (event.data !== null) {
       void self.registration.showNotification(event.data, {
         body: 'PWA Sample notification.',
-        icon: '/img/icons/house.svg'
+        icon: window.location.href.indexOf("https://koppesan-2github.io")==-1?"/img/icons/circle-play.svg":"/lyricviewer/img/icons/circle-play.svg"
       })
     }
   })
